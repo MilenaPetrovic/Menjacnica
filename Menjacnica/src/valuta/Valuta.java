3cbc9a14a4690b961f78fe0dfcd5ed238091ca54
MilenaPetrovic <miskpet@gmail.com>
2018-03-10 13:25:17 +0100

    Napravljene klase Menjacnica i Valuta

---
+++ Menjacnica/src/valuta/Valuta.java
@@ -0,0 +1,49 @@
+package valuta;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+	private String naziv, skraceniNaziv;
+	private GregorianCalendar datum;
+	private double prodajniKurs, kupovniKurs, srednjiKurs;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	
+	
+	
+}

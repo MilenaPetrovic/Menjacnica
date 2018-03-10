7d429acb2037ca8f4cbf8f0010064e2573c81044
MilenaPetrovic <miskpet@gmail.com>
2018-03-10 14:22:14 +0100

    Implementiran menjacnicaInterfejs u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,5 +1,28 @@
 package menjacnica;
 
-public class Menjacnica {
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.menjacnicaInterfejs;
+import valuta.Valuta;
+
+public class Menjacnica implements menjacnicaInterfejs{
+
+	@Override
+	public void dodajKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean obrisiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Valuta pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

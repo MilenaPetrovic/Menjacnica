a7ae251f5b734a17db2dd5718f30069530dfe1c1
MilenaPetrovic <miskpet@gmail.com>
2018-03-10 14:10:03 +0100

    Napravljen interfejs menjacnice

---
+++ Menjacnica/src/menjacnica/interfejs/menjacnicaInterfejs.java
@@ -0,0 +1,12 @@
+package menjacnica.interfejs;
+
+import java.util.GregorianCalendar;
+
+import valuta.Valuta;
+
+public interface menjacnicaInterfejs {
+	
+	public void dodajKurs(Valuta valuta, GregorianCalendar datum);
+	public boolean obrisiKurs(Valuta valuta, GregorianCalendar datum);
+	public Valuta pronadjiKurs(Valuta valuta, GregorianCalendar datum);
+}

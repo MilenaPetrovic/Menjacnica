6c21c2a88bf58c1a2ada2b9d932521a3c0a4f5ed
MilenaPetrovic <miskpet@gmail.com>
2018-03-10 14:25:18 +0100

    Otvorena grana za implementaciju metoda interfejsa

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -9,19 +9,18 @@ public class Menjacnica implements menjacnicaInterfejs{
 
 	@Override
 	public void dodajKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		
+				
 	}
 
 	@Override
 	public boolean obrisiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public Valuta pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 

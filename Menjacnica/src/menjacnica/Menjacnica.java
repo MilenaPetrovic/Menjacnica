abf4bb2d8f7c0422b4d0c16a14e8e5d73cf1542e
MilenaPetrovic <miskpet@gmail.com>
2018-03-10 17:35:29 +0100

    Implementacija metoda klase Menjacnica u master grani

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,28 +1,30 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.menjacnicaInterfejs;
 import valuta.Valuta;
 
 public class Menjacnica implements menjacnicaInterfejs{
-
+	LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
+	
 	@Override
-	public void dodajKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		
+	public void dodajKurs(Valuta valuta) {
+		kursnaLista.add(valuta);
 	}
 
 	@Override
-	public boolean obrisiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return false;
+	public void obrisiKurs(Valuta valuta) {
+		kursnaLista.remove(valuta);
 	}
 
 	@Override
-	public Valuta pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+	public Valuta pronadjiKurs(String naziv, GregorianCalendar datum) {
+		for(int i = 0; i < kursnaLista.size(); i++)
+			if(kursnaLista.get(i).getNaziv().equals(naziv) && kursnaLista.get(i).getDatum().equals(datum))
+				return kursnaLista.get(i);
+		throw new RuntimeException("U kursnoj listi ne postoji kurs za unet naziv valute i datum");
 	}
 
 }
--- Menjacnica/src/menjacnica/interfejs/menjacnicaInterfejs.java
+++ Menjacnica/src/menjacnica/interfejs/menjacnicaInterfejs.java
@@ -6,7 +6,7 @@ import valuta.Valuta;
 
 public interface menjacnicaInterfejs {
 	
-	public void dodajKurs(Valuta valuta, GregorianCalendar datum);
-	public boolean obrisiKurs(Valuta valuta, GregorianCalendar datum);
-	public Valuta pronadjiKurs(Valuta valuta, GregorianCalendar datum);
+	public void dodajKurs(Valuta valuta);
+	public void obrisiKurs(Valuta valuta);
+	public Valuta pronadjiKurs(String naziv, GregorianCalendar datum);
 }
